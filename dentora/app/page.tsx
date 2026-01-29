5d0530823da5e19ecd3f22eec246f381c975906d
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-29 15:42:17 +0700

    add hero section with content, images, and buttons to home page

--- dentora/app/page.tsx
+++ dentora/app/page.tsx
@@ -1,3 +1,73 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="text-4xl font-semibold">Home Page...</h1>;
+  return (
+    <>
+      {/* Hero */}
+      <section className="relative pt-32">
+        <div className="container grid gap-16 space-y-10 sm:space-y-0 lg:grid-cols-2 lg:items-end">
+          <div className="my-auto max-w-xl pb-10 md:max-w-2xl lg:max-w-none">
+            <p className="subtitle">Smile with Confidence</p>
+            <h1 className="text-4xl font-bold md:text-5xl lg:text-[54px]">
+              HELPING YOU BRING BACK YOUR{" "}
+              <span className="text-primary-600">HAPPY SMILE </span>
+            </h1>
+            <p className="py-[14px_32px]">
+              Lorem ipsum dolor sit amet consectetur. Consequat pharetra
+              ultrices scelerisque arcu lorem augue. Lacus justo euismod justo
+              amet dictum vel sapien est. Imperdiet tempor.
+            </p>
+
+            <div className="flex flex-col gap-4 md:flex-row">
+              <Link href={"/about"} className="primary-btn">
+                LEARN MORE
+              </Link>
+              <Link href={"/services"} className="secondary-btn">
+                BROWSE SERVICES
+              </Link>
+            </div>
+          </div>
+
+          <div className="relative max-w-max lg:mx-0 lg:ml-auto">
+            <Image
+              src={"/images/hero-img.png"}
+              alt={"hero image"}
+              width={727}
+              height={787}
+              className="relative z-20"
+            />
+            <Image
+              src={"/images/hero-card.png"}
+              alt={"hero card"}
+              width={229}
+              height={109}
+              className="ring-primary-200 absolute top-11 left-5 z-10 rounded-lg ring"
+            />
+          </div>
+        </div>
+
+        <div className="bg-primary-100 absolute right-0 bottom-0 -z-10 h-5/12 w-3/4 rounded-tl-xl sm:h-1/2 lg:h-full lg:w-1/2 lg:rounded-tl-none" />
+
+        <div>
+          <Image
+            src={"/images/shape-1.svg"}
+            alt={"shape"}
+            width={200}
+            height={200}
+            className="absolute top-28 -left-8 -z-10"
+          />
+        </div>
+      </section>
+
+      {/* Service */}
+      <section className="section">
+        <div className="container">
+          <div>
+            <p className="subtitle"></p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
