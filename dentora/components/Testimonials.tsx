5a1cbd0a3f568b62996c31b07ab40075c849c3ad
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-29 23:58:04 +0700

    implement Swiper for testimonials section with navigation buttons

--- dentora/components/Testimonials.tsx
+++ dentora/components/Testimonials.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import { testimonialsItems } from "@/data/data";
 import Image from "next/image";
 import { RiArrowLeftLine, RiArrowRightLine } from "@remixicon/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
 
 const Testimonials = () => {
   return (
@@ -13,36 +18,46 @@ const Testimonials = () => {
           Lorem ipsum dolor sit amet consectetur. Amet platea egestas aliquam
           habitant. Hac
         </p>
-        <div className="mt-11">
-          {testimonialsItems.map((item) => (
-            <div
-              key={item.id}
-              className="border-primary-200 flex flex-col items-start gap-5 overflow-hidden rounded-md border p-5 sm:flex-row"
-            >
-              <div className="max-w-max shrink-0">
-                <Image
-                  src={item.img}
-                  alt={item.author}
-                  width={95}
-                  height={95}
-                  className="ring-primary-600 size-16 rounded-full ring-4"
-                />
-              </div>
-              <div className="space-y-4">
-                <p>{item.text}</p>
-                <p className="card-title">{item.author}</p>
-              </div>
+      </div>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={20}
+        navigation={{
+          prevEl: ".prev-btn",
+          nextEl: ".next-btn",
+        }}
+        loop={true}
+        breakpoints={{ 768: { slidesPerView: 2 } }}
+        className="mt-11"
+      >
+        {testimonialsItems.map((item) => (
+          <SwiperSlide
+            key={item.id}
+            className="border-primary-200 flex flex-col items-start gap-5 overflow-hidden rounded-md border p-5 sm:flex-row"
+          >
+            <div className="max-w-max shrink-0">
+              <Image
+                src={item.img}
+                alt={item.author}
+                width={95}
+                height={95}
+                className="ring-primary-600 size-16 rounded-full ring-4"
+              />
             </div>
-          ))}
-        </div>
-        <div className="mt-11 flex items-center justify-center gap-2.5 lg:mt-16">
-          <button className="bg-primary-600 hover:bg-primary-700 border-primary-800 focus:bg-primary-700 flex size-10 items-center justify-center rounded-full border text-white transition-colors">
-            <RiArrowLeftLine />
-          </button>
-          <button className="bg-primary-600 hover:bg-primary-700 border-primary-800 focus:bg-primary-700 flex size-10 items-center justify-center rounded-full border text-white transition-colors">
-            <RiArrowRightLine />
-          </button>
-        </div>
+            <div className="space-y-4">
+              <p>{item.text}</p>
+              <p className="card-title">{item.author}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="mt-11 flex items-center justify-center gap-2.5 lg:mt-16">
+        <button className="bg-primary-600 hover:bg-primary-700 border-primary-800 focus:bg-primary-700 prev-btn flex size-10 items-center justify-center rounded-full border text-white transition-colors">
+          <RiArrowLeftLine />
+        </button>
+        <button className="bg-primary-600 hover:bg-primary-700 border-primary-800 focus:bg-primary-700 next-btn flex size-10 items-center justify-center rounded-full border text-white transition-colors">
+          <RiArrowRightLine />
+        </button>
       </div>
     </div>
   );
