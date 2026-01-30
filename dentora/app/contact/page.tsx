0fbf38adc4a7d999005e9d83dc32509888171f72
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-30 16:22:11 +0700

    add: enhance InputField and form layout with improved styling and structure

--- dentora/app/contact/page.tsx
+++ dentora/app/contact/page.tsx
@@ -5,33 +5,38 @@ const page = () => {
   return (
     <>
       <section className="section">
-        <div className="container">
+        <div className="container grid items-start gap-14 lg:grid-cols-[0.7fr_1fr]">
           <div>
-            <div>
+            <div className="space-y-3">
               <h2 className="title">Book an Appointment</h2>
-              <p className="">
+              <p className="max-w-lg">
                 Lorem ipsum dolor sit amet consectetur. Eget ligula pharetra
                 vulputate ac dictumst. Lacus vitae diam.
               </p>
             </div>
-            <div>
-              <h3 className="card-title">More information</h3>
-              <ul className="">
+            <div className="mt-4">
+              <h3 className="card-title mb-5">More information</h3>
+              <ul className="space-y-5">
                 {contactListItems.map((item) => (
-                  <li key={item.id} className="">
-                    <span>
+                  <li key={item.id} className="flex items-start gap-2.5">
+                    <span className="bg-primary-100 text-primary-600 flex size-12 shrink-0 items-center justify-center rounded-lg">
                       <item.icon size={30} />
                     </span>
-                    <div className="card-title">{item.title}</div>
-                    <p>{item.text}</p>
+                    <div>
+                      <div className="card-title">{item.title}</div>
+                      <p>{item.text}</p>
+                    </div>
                   </li>
                 ))}
               </ul>
             </div>
           </div>
 
-          <form action="">
-            <div>
+          <form
+            action=""
+            className="border-primary-200 space-y-7 rounded-lg border px-6 py-9"
+          >
+            <div className="grid gap-5 md:grid-cols-2">
               <InputField
                 label={"Name"}
                 placeholder={"Full Name"}
@@ -44,17 +49,17 @@ const page = () => {
               />
             </div>
 
-            <div>
+            <div className="grid gap-5 md:grid-cols-2">
               <InputField
                 label={"Phone Number"}
                 placeholder={"(123)-456-8900"}
                 type={"tel"}
               />
 
-              <div>
-                <label>Services</label>
-                <div>
-                  <select>
+              <div className="grid gap-2">
+                <label className="text-lg font-semibold">Services</label>
+                <div className="border-primary-400 bg-primary-100/20 hover:bg-primary-100/35 rounded-lg border px-5 py-3 placeholder-gray-600">
+                  <select className="h-full w-full outline-none">
                     {formDropdownItems.map((item, index) => (
                       <option key={index}>{item}</option>
                     ))}
@@ -63,9 +68,23 @@ const page = () => {
               </div>
             </div>
 
-            <button className="primary-btn">Submit</button>
+            <div className="grid gap-2">
+              <label className="text-lg font-semibold">Message</label>
+              <textarea
+                placeholder={"please describe what service you are in."}
+                className="border-primary-400 bg-primary-100/20 hover:bg-primary-100/35 h-[158px] w-full resize-none rounded-lg border px-5 py-3 placeholder-gray-600"
+              />
+            </div>
+
+            <button className="primary-btn tracking-wider uppercase">
+              Submit
+            </button>
           </form>
         </div>
+      </section>
+
+      <section className="section">
+        <div className="container"></div>
       </section>
     </>
   );
--- dentora/components/InputField.tsx
+++ dentora/components/InputField.tsx
@@ -2,9 +2,13 @@ import { inputFieldProps } from "@/types/types";
 
 const InputField = ({ label, placeholder, type }: inputFieldProps) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input type={type} placeholder={placeholder} />
+    <div className="grid gap-2">
+      <label className="text-lg font-semibold">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="border-primary-400 bg-primary-100/20 hover:bg-primary-100/35 rounded-lg border px-5 py-3 placeholder-gray-600"
+      />
     </div>
   );
 };
