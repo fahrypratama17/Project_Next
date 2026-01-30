c45ee446f94c1222029c81a7c2cc1bf1e20a33fd
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-30 13:39:11 +0700

    add: implement Services page with service descriptions and booking link

--- dentora/app/services/page.tsx
+++ dentora/app/services/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import Link from "next/link";
+import { servicesItems } from "@/data/data";
+import ServicesCard from "@/components/ServicesCard";
+import Cta from "@/components/Cta";
 
 const page = () => {
-  return <div>Services Page</div>;
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <div className="flex flex-wrap items-center justify-between gap-6">
+            <div className="space-y-6">
+              <h2 className="title">Our Services</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Eget ligula pharetra
+                vulputate ac dictumst. Lacus vitae diam.
+              </p>
+            </div>
+
+            <Link href={"/contact"} className="primary-btn">
+              BOOK AN APPOINTMENT
+            </Link>
+          </div>
+
+          <div className="mt-16 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {servicesItems.map((item) => (
+              <ServicesCard key={item.id} {...item} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <Cta />
+      </section>
+    </>
+  );
 };
 
 export default page;
